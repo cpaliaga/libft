8f667ae94407e6313cbbf0246c42ee6e9c1eafb5
Cristina Patricia Aliaga Jimenez <caliaga-@c2r11s6.42madrid.com>
2022-10-11 16:04:32 +0200

    mar1604

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,48 +6,36 @@
 /*   By: caliaga- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 20:24:38 by caliaga-          #+#    #+#             */
-/*   Updated: 2022/10/10 20:04:10 by caliaga-         ###   ########.fr       */
+/*   Updated: 2022/10/11 16:01:09 by caliaga-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_w(size_t	h, char *hay, size_t n, char *need)
-{
-	if ((*need + n) != '\0')
-		if (*(hay + h + n) == *(need + n))
-			return (1);
-	return (0);
-}
-
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	h;
 	size_t	n;
-	size_t	x;
-	char	*hay;
-	char	*need;
+	size_t	len_need;
 
-	hay = (char *)haystack;
-	need = (char *)needle;
-
-	if (*need == '\0')
-		return (hay);
+	len_need = ft_strlen(needle);
+	if (*needle == '\0')
+		return ((char *)haystack);
 	h = 0;
-	// que len no sea 0 y exista 'hay'  
-	while (len-- > 0 && hay[h] != '\0')
+	while (*(haystack + h) != '\0' && h <= len)
 	{
 		n = 0;
-		if (*hay == *need)
+		if (*(haystack + h) == *needle)
 		{
-			x = h + n; 
-			while (need[n] != '\0' && hay[x] == need[n])  }
-					ft_w(h, *hay, n, *need, len) == 1
-					)
-				n++;
+			while (n < len_need)
+			{
+			    if (*(haystack + h + n) == *(needle + n))
+			        if (*(needle + n) != '\0')
+			            n++;
+			}
+			if (*(needle + n) == '\0')
+				return ((char *)(haystack + h));
 		}
-		if (*(need + n) == '\0')
-			return ((hay + h));}		
 		h++;
 	}
 	return (0);
