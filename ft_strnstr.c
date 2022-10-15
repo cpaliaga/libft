faa40c4f05d8c1b4c8f2563e48097aca9f0da76d
Cristina Patricia Aliaga Jimenez <caliaga-@c3r8s1.42madrid.com>
2022-10-15 20:29:56 +0200

    sb2029

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: caliaga- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 20:24:38 by caliaga-          #+#    #+#             */
-/*   Updated: 2022/10/14 16:25:27 by caliaga-         ###   ########.fr       */
+/*   Updated: 2022/10/15 20:15:26 by caliaga-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t	len_needle;
+	size_t	nlen;
 
+	nlen = ft_strlen(needle);
 	if (!*needle)
 		return ((char *)haystack);
-	len_needle = ft_strlen(needle);
 	while (*haystack != '\0' && len-- > 0)
 	{
 		if (*haystack == *needle)
-			if (!ft_strncmp(haystack, needle, len_needle))
+			if (!ft_strncmp(haystack, needle, nlen))
 				return ((char *)haystack);
+		if ((unsigned int)nlen > (unsigned int)len)
+			return (0);
 		haystack++;
-		if (len_needle > len)
-			break ;
 	}
 	return (0);
 }
