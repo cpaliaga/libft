9dcc0dfe3d8aa0f71e0eae4c8712b520051212ee
Cristina Patricia Aliaga Jimenez <caliaga-@c1r12s1.42madrid.com>
2022-11-03 14:00:29 +0100

    jue1400

--- ft_split.c
+++ ft_split.c
@@ -6,38 +6,50 @@
 /*   By: caliaga- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 17:23:25 by caliaga-          #+#    #+#             */
-/*   Updated: 2022/10/28 17:50:35 by caliaga-         ###   ########.fr       */
+/*   Updated: 2022/11/03 13:58:08 by caliaga-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	ft_rows(char const *s, char c)
+static size_t	*ft_rows(char const *s, char c, size_t *rows)
+{
+	size_t	nb;
+	size_t	i;
+	size_t	k;
+	size_t	*f;
+
+	i = -1;
+	nb = 0;
+	while (s[++i] != '\0')
+		if (s[i] != c)
+			if (i==0 || s[i -1] == c)
+				nb++;
+	*rows = nb;
+	i = -1;
+	k = -1;
+	f = malloc (nb * sizeof(size_t));
+	while (s[++i] != '\0')
+		if (s[i] != c)
+			if (i==0 || s[i -1] == c)
+				 f[++k] = i;
+	return (f);
+}
+
+static void	ft_cols(const char *s, char c, size_t start, size_t *cols)
 {
 	size_t	nb;
 
-	nb = 1;
-	while (*s == c)
-		s++;
-	while (*s)
+	nb = 0;
+	while (s[start] != c && s[start] != '\0')
 	{
-		if (*s == c && *(s - 1) != c)
-				nb += 1;
-		s++;
+		start++;
+		nb++;
 	}
-	return (nb);
+	*cols = nb;
 }
 
-static void	ft_cols(const char *s, size_t *k, size_t *cols, char c)
-{
-	while (s[*k] != c && s[*k] != '\0')
-	{
-		(*k)++;
-		(*cols)++;
-	}
-}
-
-static void ft_check(char **split, size_t row)
+static void	ft_check(char **split, size_t row)
 {
 	while (row-- > 0)
 	{
@@ -48,25 +60,23 @@ static void ft_check(char **split, size_t row)
 
 char	**ft_split(char const *s, char c)
 {
+	size_t	rows;
 	size_t	row;
 	size_t	cols;
-	size_t	k;
+	size_t	*starts;
 	char	**split;
 
+	starts = ft_rows(s,c,&rows);
 	row = 0;
-	cols = 0;
-	k = 0;
 	if (!s)
 		return (NULL);
-	split = (char **) malloc ((ft_rows(s, c) + 1) * sizeof(char));
+	split = (char **) malloc ((rows + 1) * sizeof(char));
 	if (!split)
 		return (NULL);
-	while (row < ft_rows(s, c))
+	while (row < rows)
 	{
-		while (s[k] == c && s[k] != '\0')
-			k++;
-		ft_cols(s, &k, &cols, c);
-		split[row] = ft_substr(s, (k - cols), cols);
+		ft_cols(s, c, starts[row], &cols);
+		split[row] = ft_substr(s, starts[row], cols);
 		if (!split[row])
 			ft_check(split, row);
 		if (!split[row])
