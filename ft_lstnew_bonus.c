103aadf522279fedbf97bf16ab706b49ba7ceea4
Cristina Patricia Aliaga Jimenez <caliaga-@c3r8s3.42madrid.com>
2022-12-12 15:06:33 +0100

    dic

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -6,7 +6,7 @@
 /*   By: caliaga- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 14:36:02 by caliaga-          #+#    #+#             */
-/*   Updated: 2022/12/06 16:14:19 by caliaga-         ###   ########.fr       */
+/*   Updated: 2022/12/12 13:53:50 by caliaga-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_list	*ft_lstnew(void *content)
 	t_list	*rtn;
 	t_list	*sure;
 
-	rtn = (t_list *)malloc(sizeof(t_list));
+	rtn = malloc(sizeof(t_list));
 	if (!rtn)
 		return (rtn = NULL);
 	sure = rtn;
