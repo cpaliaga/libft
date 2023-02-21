8e9e9410a48d1f53743e9313b2a93f6c28adddf0
Cristina Patricia Aliaga Jimenez <caliaga-@c1r9s2.42madrid.com>
2023-02-21 13:46:40 +0100

    martes21a1_46

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: caliaga- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 18:08:11 by caliaga-          #+#    #+#             */
-/*   Updated: 2022/12/06 15:38:48 by caliaga-         ###   ########.fr       */
+/*   Updated: 2022/12/12 15:42:22 by caliaga-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,19 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	len_s;
 	size_t	len_d;
 
-	len_d = ft_strlen(dst);
 	len_s = ft_strlen(src);
-	if (!src)
-		len_s = 0;
-	if (dstsize == 0)
-		return (len_s);
-	if (dstsize < len_d)
-		len_s = ft_strlen(src) + dstsize;
-	else
-		len_s = ft_strlen(src) + len_d;
-	i = -1;
-	if (dst != NULL)
+	len_d = ft_strlen(dst);
+	i = 0;
+	if (dstsize != 0 && len_d < dstsize)
 	{
-		while (src[++i] != '\0' && (len_d + i) < dstsize - 1)
-			dst[len_d + i] = src[i];
-		dst[len_d + i] = '\0';
+		while ((src[i] != '\0') && i < (dstsize - len_d - 1))
+		{
+			*(dst + len_d + i) = *(src + i);
+			i++;
+		}
+		*(dst + len_d + i) = '\0';
 	}
-	return (len_s);
+	if (len_d >= dstsize)
+		return (len_s + dstsize);
+	return (len_s + len_d);
 }
